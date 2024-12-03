ac9b819ba0ea51f69f1b85759a3ba1dc70e58d3f
mattglbrt <matt@everyway.io>
2024-12-03 12:55:36 -0500

    Homepage Updates

--- src/consts.ts
+++ src/consts.ts
@@ -1,36 +1,30 @@
 import type { Metadata, Site } from "@types";
 
 export const SITE: Site = {
-  TITLE: "Astro Micro Academics",
-  DESCRIPTION: "Astro Micro Academics is for academic user.",
-  EMAIL: "youremial@gmail.com",
-  NUM_POSTS_ON_HOMEPAGE: 2,
+  TITLE: "The Book of Valtiare",
+  DESCRIPTION: "Following the muse.",
+  EMAIL: "valtiare@gmail.com",
+  NUM_POSTS_ON_HOMEPAGE: 10,
   NUM_PUBLICATIONS_ON_HOMEPAGE: 3,
-  SITEURL: 'https://astro-micro-academic.vercel.app' // Update here to link the RSS icon to your website rss
+  SITEURL: 'https://valtiare.com' // Update here to link the RSS icon to your website rss
 };
 
-export const HIGHLIGHTAUTHOR = "John B"
+export const HIGHLIGHTAUTHOR = "Valtiare"
 
 export const HOME: Metadata = {
   TITLE: "Home",
-  DESCRIPTION: "Astro Micro is an accessible theme for Astro.",
+  DESCRIPTION: "The Book of Valtiare",
 };
 
 export const BLOG: Metadata = {
   TITLE: "Blog",
-  DESCRIPTION: "A collection of articles on topics I am passionate about.",
+  DESCRIPTION: "A collection of research notes, ramblings, and smaller articles.",
 };
 
 export const RESEARCH: Metadata = {
   TITLE: "Publications",
   DESCRIPTION:
     "A collection of my publications with links to paper, repositories and live demos.",
-};
-
-export const CV: Metadata = {
-  TITLE: "CV",
-  DESCRIPTION:
-    "your cv",
 };
 
 export const TAGS: Metadata = {
